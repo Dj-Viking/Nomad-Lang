34d8baac73794cd20314e8e747c8f577102dcdca
dj-viking <anders.swedishviking@gmail.com>
2021-10-22 12:22:03 -0400

    able to collapse and open nav bar

--- client/src/components/SideBar.vue
+++ client/src/components/SideBar.vue
@@ -1,34 +1,81 @@
 <template>
-  <div class="side-bar">
-    <div style="display: flex; justify-content: flex-start">
-      <div style="display: flex; flex-direction: column">
-        <div style="display: flex; flex-direction: row">
-          <h4 style="font-size: 20px" class="title">Your Categories</h4>
-          <i
-            @click.prevent="
-              ($event) => {
-                toggleSideBar($event);
-              }
+  <div class="side-bar" :class="{ open: sidebarOpen, closed: !sidebarOpen }">
+    <div style="display: flex; flex-direction: column; min-width: 200px">
+      <Transition type="transition" name="fade" mode="out-in">
+        <div v-if="sidebarOpen">
+          <div
+            style="
+              display: flex;
+              justify-content: center;
+              flex-direction: column;
             "
-            style="cursor: pointer; margin-top: 1em"
-            :class="{
-              'fa fa-chevron-left big': sidebarOpen,
-              'fa fa-chevron-right big': !sidebarOpen,
-            }"
-            aria-hidden="true"
-          ></i>
+            mode="out-in"
+          >
+            <i
+              @click.prevent="
+                ($event) => {
+                  toggleSideBar($event);
+                }
+              "
+              style="
+                cursor: pointer;
+                margin-top: 0.5em;
+                margin-left: 0.5em;
+                margin-right: 0.5em;
+              "
+              :class="{
+                'fa fa-chevron-left big': sidebarOpen,
+                'fa fa-chevron-right big': !sidebarOpen,
+              }"
+              aria-hidden="true"
+            ></i>
+            <h4
+              style="
+                font-size: 20px;
+                margin-bottom: 1em;
+                margin-top: 1em;
+                margin-left: 10px;
+                height: 30px;
+              "
+              class="title"
+            >
+              Your Categories
+            </h4>
+          </div>
+          <div v-for="(key, i) of Object.keys(categories)" :key="i">
+            <SideBarNode
+              :id="categories[key].id?.toString()"
+              :allCards="allCards"
+              :isActive="categories[key].isActive"
+              :categoryName="key"
+              :categories="categories"
+              :categorizedCards="categories[key].cards"
+            />
+          </div>
         </div>
-        <div v-for="(key, i) of Object.keys(categories)" :key="i">
-          <SideBarNode
-            :id="categories[key].id?.toString()"
-            :allCards="allCards"
-            :isActive="categories[key].isActive"
-            :categoryName="key"
-            :categories="categories"
-            :categorizedCards="categories[key].cards"
-          />
+        <div v-else>
+          <div style="display: flex; flex-direction: row">
+            <i
+              @click.prevent="
+                ($event) => {
+                  toggleSideBar($event);
+                }
+              "
+              style="
+                cursor: pointer;
+                margin-top: 0.5em;
+                margin-left: 0.5em;
+                margin-right: 0.5em;
+              "
+              :class="{
+                'fa fa-chevron-left big': sidebarOpen,
+                'fa fa-chevron-right big': !sidebarOpen,
+              }"
+              aria-hidden="true"
+            ></i>
+          </div>
         </div>
-      </div>
+      </Transition>
     </div>
   </div>
 </template>
@@ -87,11 +134,31 @@ export default defineComponent({
   position: fixed;
   top: 0;
   height: 100vh;
-  width: 20vh;
+
   z-index: 10000;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity height 0.2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  height: 0;
 }
 
 .big {
   font-size: 30px;
+}
+
+.open {
+  width: 200px;
+  transition: 0.2s;
+}
+
+.closed {
+  width: 0px;
+  transition: 0.1s ease 0.3s;
 }
 </style>
