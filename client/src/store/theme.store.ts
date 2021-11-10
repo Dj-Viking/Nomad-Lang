a58b37d58df9de43c901b2c0bae14c53cfa42c23
dj-viking <anders.swedishviking@gmail.com>
2021-11-10 00:01:54 -0500

    refactor

--- client/src/store/theme.store.ts
+++ client/src/store/theme.store.ts
@@ -1,4 +1,3 @@
-/* eslint-disable */ 
 import { ThemeState } from "@/types";
 const state = {
   theme: window.localStorage.getItem("theme") || "light",
@@ -16,20 +15,26 @@ const mutations = {
           window.localStorage.setItem("theme", "dark");
           state.theme = "dark";
 
+          // eslint-disable-next-line
           document.querySelector("html")!.style.transition = "0.5s";
+          // eslint-disable-next-line
           document.querySelector("html")!.style.backgroundColor = "#222222";
           document.body.classList.remove("body-light");
           document.body.classList.add("body-dark");
         }
         break;
       case state.theme === "dark":
-        window.localStorage.setItem("theme", "light");
-        state.theme = "light";
+        {
+          window.localStorage.setItem("theme", "light");
+          state.theme = "light";
 
-        document.querySelector("html")!.style.transition = "0.5s";
-        document.querySelector("html")!.style.backgroundColor = "white";
-        document.body.classList.remove("body-dark");
-        document.body.classList.add("body-light");
+          // eslint-disable-next-line
+            document.querySelector("html")!.style.transition = "0.5s";
+          // eslint-disable-next-line
+          document.querySelector("html")!.style.backgroundColor = "white";
+          document.body.classList.remove("body-dark");
+          document.body.classList.add("body-light");
+        }
         break;
     }
   },
