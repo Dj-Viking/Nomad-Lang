3fe84055fa4498ecb63a62ade961cde2c23e136c
dj-viking <anders.swedishviking@gmail.com>
2022-03-11 11:15:57 -0500

    committing troubleshooting changes

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
-export default mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/rest-cats", {
-  autoIndex: true,
-});
+export default mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/rest-cats", {
+    autoIndex: true,
+  })
+  .catch((e) => console.log("error on connection", e));
