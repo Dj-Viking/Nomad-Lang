79743e123d3c39a135fe785f6db6d7628c8961a5
dj-viking <anders.swedishviking@gmail.com>
2022-03-21 16:30:51 -0400

    fix broken get route

--- server/dist/router/index.js
+++ server/dist/router/index.js
@@ -7,7 +7,7 @@ const path_1 = __importDefault(require("path"));
 const express_1 = require("express");
 const userRouter_1 = require("./userRouter");
 const router = (0, express_1.Router)();
-router.get("*", (_req, res) => {
+router.get("/", (_req, res) => {
     res.sendFile(path_1.default.join(__dirname, "../../../client/dist/index.html"));
 });
 router.use("/user", userRouter_1.userRouter);
--- server/src/router/index.ts
+++ server/src/router/index.ts
@@ -2,7 +2,7 @@ import path from "path";
 import { Router } from "express";
 import { userRouter } from "./userRouter";
 const router = Router();
-router.get("*", (_req, res) => {
+router.get("/", (_req, res) => {
   res.sendFile(path.join(__dirname, "../../../client/dist/index.html"));
 });
 router.use("/user", userRouter);
