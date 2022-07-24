28ef4ac2cdcfb6a8759767dd02b8d1237638f022
dj-viking <anders.swedishviking@gmail.com>
2022-07-24 14:40:28 -0400

    more refactor

--- client/src/components/ChoiceButton.vue
+++ client/src/components/ChoiceButton.vue
@@ -148,7 +148,6 @@ export default defineComponent({
                             return char;
                         }
                     })
-                    .filter((item) => item !== null)
                     .join("");
                 return new_str;
             } else {
