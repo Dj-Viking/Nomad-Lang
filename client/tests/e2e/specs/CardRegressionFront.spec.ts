fc166011ee3e1429c1a55ac00faa1e95837c6e8d
dj-viking <anders.swedishviking@gmail.com>
2022-08-20 14:42:23 -0400

    add intercepts to card regression front

--- client/tests/e2e/specs/CardRegressionFront.spec.ts
+++ client/tests/e2e/specs/CardRegressionFront.spec.ts
@@ -1,17 +1,16 @@
 import {
-  REGISTER_USERNAME,
-  REGISTER_EMAIL,
-  REGISTER_PASSWORD,
   EXPECTED_ADD_LOCAL_CARD_OBJECT,
   CARD_FRONT_REGRESSION_SPEC_PATH,
   CARD_FRONT_REGRESSION_SPEC_PATH_HEADLESS,
   DIFF_FIXTURE_FOLDER_PATH,
   BASE_CARD_FRONT_FIXTURE,
   ACTUAL_CARD_FRONT_FIXTURE,
-  CARD_FRONT_SCREENSHOT_FILE_NAME
+  CARD_FRONT_SCREENSHOT_FILE_NAME,
+  MOCK_USER
 } from "../../constants";
 import { PNG, PNGWithMetadata } from "pngjs";
 import Pixelmatch from "pixelmatch";
+import { IMeResponse } from "../../../../server/src/types";
 
 let baselinePng: PNGWithMetadata;
 const baseDimensions = {
@@ -25,10 +24,6 @@ const actualDimensions = {
 };
 let diff: PNG;
 let matchNum = 0;
-
-let unique_username = "";
-let unique_email = "";
-let token: string | null = "";
 
 
 beforeEach(() => cy.restoreLocalStorage());
@@ -53,48 +48,26 @@ describe("visits home page", () => {
 });
 
 describe("sign up new user", () => {
-  //sign in as new user
-  //check that the cards are empty for a newly signed in user comes to home page
-  it("clicks signup link ", () => {
-    cy.get("a.button.is-success").contains("Signup").click();
-  });
-  it("types in username", () => {
-    unique_username = `${REGISTER_USERNAME}-${Date.now()}`;
-    cy.get("input[name=username]")
-      .should("have.length", 1)
-      .type(unique_username);
-  });
-  it("types in email", () => {
-    unique_email = `${REGISTER_EMAIL}-${Date.now()}`;
-    cy.get("input[name=email]").should("have.length", 1).type(unique_email);
-  });
-  it("types in password", () => {
-    cy.get("input[name=password]")
-      .should("have.length", 1)
-      .type(REGISTER_PASSWORD);
-  });
-  it("clicks the submit button", () => {
-    cy.get("button").contains("Sign Up!").should("have.length", 1).click();
-    cy.wait(2000);
-    // eslint-disable-next-line
-    // @ts-ignore //this is ignored because I didn't make the type yet
-    cy.saveLocalStorage();
-  });
-  it("checks that success message appears ", () => {
-    cy.get("div.Vue-Toastification__toast-body").should("have.length", 1);
-  });
-  it("waits a bit and checks we are back at the home page, i.e. checking if the add card button is on the page, and that local storage has a token, and localstorage has a global email set DO ALL LOGGED IN CARDS FEATURES", () => {
-    cy.wait(2000);
-    cy.get("button").contains("Add New Card");
+  it("signs up user", () => {
+    cy.intercept("**/user/signup", (req) => {
+      req.reply(MOCK_USER);
+    }).as("signup");
+    cy.intercept("**/user/me", (req) => {
+      req.reply({
+        user: {
+          ...MOCK_USER,
+          cards: [...MOCK_USER.cards, EXPECTED_ADD_LOCAL_CARD_OBJECT]
+        }
+      } as IMeResponse);
+    }).as("me");
+    cy.signup();
     //not sure why the assertion only works here but okay
     // cypress trashes local storage during the test to prevent buildup of state or something like that
-    cy.window().then((window: Cypress.AUTWindow) => {
-      console.log("what is the token here", token);
+    let token: string | null = "";
+    cy.restoreLocalStorage().wait("@signup").wait("@me");
+    cy.window().then((window) => {
       token = window.localStorage.getItem("id_token");
-      console.log("what is the token here", token);
       expect(token).to.not.be.null;
-      // eslint-disable-next-line
-      // @ts-ignore //this is ignored because I didn't make the type yet
       cy.saveLocalStorage();
     });
   });
@@ -102,31 +75,20 @@ describe("sign up new user", () => {
 
 describe("add a card to screenshot the front", () => {
   it("adds one card", () => {
-    //open the modal
-    cy.get("button").contains("Add New Card").click();
-    //select input fields and type
-    cy.get("input[name=modalAddFsText]").type(
-      EXPECTED_ADD_LOCAL_CARD_OBJECT.frontSideText as string
-    );
-    cy.get("input[name=modalAddFsTextLanguage]").type(
-      EXPECTED_ADD_LOCAL_CARD_OBJECT.frontSideLanguage as string
-    );
-    cy.get("input[name=modalAddFsTextPicture]").type(
-      EXPECTED_ADD_LOCAL_CARD_OBJECT.frontSidePicture as string
-    );
-    cy.get("input[name=modalAddBsText]").type(
-      EXPECTED_ADD_LOCAL_CARD_OBJECT.backSideText as string
-    );
-    cy.get("input[name=modalAddBsTextLanguage]").type(
-      EXPECTED_ADD_LOCAL_CARD_OBJECT.backSideLanguage as string
-    );
-    cy.get("input[name=modalAddBsTextPicture]").type(
-      EXPECTED_ADD_LOCAL_CARD_OBJECT.backSidePicture as string
-    );
-    //get the submit add button
-    cy.get("button[name=submitAddCard]").click();
-    cy.wait(2000);
-    //add a card finish
+    cy.intercept("**/user/addCard", (req) => {
+      req.reply({
+        cards: [EXPECTED_ADD_LOCAL_CARD_OBJECT]
+      });
+    }).as("addCard");
+    cy.intercept("**/user/me", (req) => {
+      req.reply({
+        user: {
+          ...MOCK_USER,
+          cards: [...MOCK_USER.cards, EXPECTED_ADD_LOCAL_CARD_OBJECT]
+        }
+      } as IMeResponse);
+    }).as("me");
+    cy.addCard();
   });
 });
 
