2884b18c010d616353fb45a4ddfc718e1b7ea025
dj-viking <anders.swedishviking@gmail.com>
2024-06-28 22:24:48 -0400

    update client

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,3 +1,9 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-ignore
+window.process ? null : window.process = {
+  env: process?.env?.NODE_ENV || "production" 
+} as any;
+
 export const API_URL =
   process.env.NODE_ENV === "production"
     ? "https://nomad-lang.onrender.com"
